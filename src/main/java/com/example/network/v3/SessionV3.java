edb27ad008edac205ccafb6cc0bbe30eed12c9b8
ShaunAUS <wnsqud70@gmail.com>
2025-01-17 16:28:56 +0900

    멀티 쓰레드를 활용한 socket 여러개 연결(클라이언트 여려개 연결)

---
+++ src/main/java/com/example/network/v3/SessionV3.java
@@ -0,0 +1,52 @@
+package com.example.network.v3;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static com.example.util.MyLogger.log;
+
+public class SessionV3 implements Runnable {
+
+    private final Socket socket;
+
+    public SessionV3(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            while (true) {
+                //클라로부터 문자 받기
+                String received = input.readUTF();
+                log("client -> server: " + received);
+
+                if (received.equals("quit")) {
+                    break;
+                }
+
+                //클라로 문자 보내기
+                String toSend = received + "World";
+                output.writeUTF(toSend);
+                log("client <- server: " + toSend);
+            }
+
+            log("연결종료" + socket);
+            input.close();
+            output.close();
+            socket.close();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
