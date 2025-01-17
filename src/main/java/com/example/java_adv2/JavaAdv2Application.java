e70f4d02fcf973723a77a1facb4e30131053dc7c
ShaunAUS <wnsqud70@gmail.com>
2025-01-17 13:57:31 +0900

    init

---
+++ src/main/java/com/example/java_adv2/JavaAdv2Application.java
@@ -0,0 +1,13 @@
+package com.example.java_adv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAdv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_adv2/JavaAdv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_adv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAdv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
